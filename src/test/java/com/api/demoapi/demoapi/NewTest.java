51ace4b5f2330a7fe96291fd18110393dc6d44cb
amit <amitdogra85@gmail.com>
2020-08-29 22:52:41 +0530

    first submit

---
+++ src/test/java/com/api/demoapi/demoapi/NewTest.java
@@ -0,0 +1,14 @@
+package com.api.demoapi.demoapi;
+
+import org.testng.annotations.*;
+import org.testng.annotations.BeforeTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
